2a4ec6f483abb6398eed7dd2cddd7ae9c9f93b2e
gradan-hash <mutukunyaa@gmail.com>
2023-09-07 23:16:30 +0300

    weights

--- running yolo/yolobasics.py
+++ running yolo/yolobasics.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 import cv2
+
+#pretrained model(nano,m-median,l-large)--- in relation to camera
 model = YOLO('../weights/yolov8n.pt')
-results = model("images/img3.jpg",show=True)
+results = model("images/img3.jpg", show=True)
 
 cv2.waitKey(0)
 
