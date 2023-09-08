670b60c0ed974b662f9b0b16c4b1ad828e9c7a66
gradan-hash <mutukunyaa@gmail.com>
2023-09-08 12:03:19 +0300

    cvzone confidene

--- yolowebcam/yolowebcam.py
+++ yolowebcam/yolowebcam.py
@@ -1,7 +1,8 @@
-
 from ultralytics import YOLO
 import cv2
 import numpy as np
+import math
+import cvzone
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
@@ -22,8 +23,17 @@ while True:
         for box in boxes:
             x1, y1, x2, y2 = box.xyxy[0]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
-            print(x1, y1, x2, y2)
-            cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)  # for open cv
+            # print(x1, y1, x2, y2)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 0, 255), 3)  # for open cv
+
+            # USING CVZONE MUCH ADVANCED AND MORE FEATURES
+            w, h = x2 - x1, y2 - y1
+            cvzone.cornerRect(img, (x1, y1, w, h))
+
+            confidence = math.ceil((box.conf[0] * 100)) / 100
+            print("confidence ", confidence)
+
+            cvzone.putTextRect(img, f"{confidence}", (x1, y1 - 20))
 
     cv2.imshow("image", img)
     cv2.waitKey(1)
